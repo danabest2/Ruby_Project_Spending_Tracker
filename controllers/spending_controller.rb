6158ca57ab417ef90b42b557b1301b94b8e974f7
DANIELA <danielamiritescu@gmail.com>
2019-07-22 16:07:23 +0100

    show spending_type tags

--- controllers/spending_controller.rb
+++ controllers/spending_controller.rb
@@ -6,6 +6,17 @@ require_relative('../models/transaction')
 also_reload('../models/*')
 
 get '/spending_types' do
-  @spending_types = Spending_types.all()
+  @spending_types = Spending_type.all()
   erb(:"spending/index")
+end
+
+
+get '/spending_types/new' do
+  @spending_types = Spending_type.all
+  erb(:"spending/new")
+end
+
+post '/spending_types' do #create
+  Spending_type.new(params).save
+  redirect to '/spending_types'
 end
